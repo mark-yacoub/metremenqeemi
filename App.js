2472e116780b0d7b20ae244e9e1f79eb06a39c45
Mark Yacoub <markyacoub@google.com>
2022-01-03 20:56:19 -0500

    Changing functions expressions to anonymous functions.

--- App.js
+++ App.js
@@ -1,70 +1,56 @@
-import 'react-native-gesture-handler';
-
 import * as React from 'react';
 import {Button, View} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.navigate('Level One')} title="Go to Level One" />
-    </View>
-  );
-}
+const HomeScreen = ({navigation}) => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Button onPress={() => navigation.navigate('Level One')} title='Go to Level One' />
+  </View>
+);
 
-function LevelOne({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.navigate('Level Two')} title="Go to Level Two" />
-    </View>
-  );
-}
+const LevelOne = ({navigation}) => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Button onPress={() => navigation.navigate('Level Two')} title='Go to Level Two' />
+  </View>
+);
 
-function LevelTwo({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.navigate('Level Three')} title="Go to Level Three" />
-    </View>
-  );
-}
+const LevelTwo = ({navigation}) => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Button onPress={() => navigation.navigate('Level Three')} title='Go to Level Three' />
+  </View>
+);
 
-function LevelThree({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.navigate('Level Four')} title="Go to Level Four" />
-    </View>
-  );
-}
+const LevelThree = ({navigation}) => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Button onPress={() => navigation.navigate('Level Four')} title='Go to Level Four' />
+  </View>
+);
 
-function LevelFour({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+const LevelFour = ({navigation}) => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Button onPress={() => navigation.goBack()} title='Go back home' />
+  </View>
+);
 
-function About({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
+const About = ({navigation}) => (
+  <View style={{flex: 1, alignItems: ' center', justifyContent: ' center'}}>
+    <Button onPress={() => navigation.goBack()} title='Go back home' />
+  </View>
+);
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Level One" component={LevelOne} />
-        <Drawer.Screen name="Level Two" component={LevelTwo} />
-        <Drawer.Screen name="Level Three" component={LevelThree} />
-        <Drawer.Screen name="Level Four" component={LevelFour} />
-        <Drawer.Screen name="About" component={About} />
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name='Home' component={HomeScreen} />
+        <Drawer.Screen name='Level One' component={LevelOne} />
+        <Drawer.Screen name='Level Two' component={LevelTwo} />
+        <Drawer.Screen name='Level Three' component={LevelThree} />
+        <Drawer.Screen name='Level Four' component={LevelFour} />
+        <Drawer.Screen name='About' component={About} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
