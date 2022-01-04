3b7a19f40db778e6d983e7126875866d10b6668f
Mark Yacoub <markyacoub@google.com>
2022-01-03 20:48:18 -0500

    Add support for navigation on iOS

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+
 import * as React from 'react';
 import {Button, View} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
