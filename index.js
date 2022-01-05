39b2a64b4496e615086ae6f16c8065386425aaeb
Mark Yacoub <markyacoub@google.com>
2022-01-05 00:40:34 -0500

    Downgrade all related navigation packages.
    react-native-reanimated 2+ has a known of issue of not working well
    with @react-navigation/drawer which requires reanimated.
    Downgrading Reanimated is not enough as there is a dance of dependencies
    needed for everything to work closely together.
    
    Issue: https://github.com/software-mansion/react-native-reanimated/issues/2301
    Versions Reference: https://github.com/software-mansion/react-native-reanimated/issues/2301#issuecomment-920967722
    
    Suggested-by: Yousef Sawires

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
  * @format
  */
 
+// Required to be here for the prod build to work:
+// https://reactnavigation.org/docs/drawer-navigator#installation
+import 'react-native-gesture-handler';
+
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
