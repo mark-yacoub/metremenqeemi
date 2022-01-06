5579a70f5a7160e65ceec4412c2f8fcb2753e2f1
Mark Yacoub <markyacoub@google.com>
2022-01-06 15:28:52 -0500

    1:2 Create lesson 2 with its letters and words

--- src/consts/letterName.js
+++ src/consts/letterName.js
@@ -1,8 +1,14 @@
 export default LETTER_NAME = {
   Ⲁ: 'Alpha',
+  Ⲉ: 'Ey',
   Ⲏ: 'Eta',
   Ⲓ: 'Yota',
+  Ⲕ: 'Kappa',
+  Ⲙ: 'Mey',
   Ⲛ: 'Ney',
+  Ⲟ: 'Omicron',
   Ⲣ: 'Rho',
+  Ⲩ: 'Upsilon',
+  Ⲱ: 'Omega',
   ⲞⲨ: 'Omicron-Upsilon',
 };
--- src/consts/letterPronunciation.js
+++ src/consts/letterPronunciation.js
@@ -1,8 +1,14 @@
 export default LETTER_PRONUNCIATION = {
   Ⲁ: 'A as in Car',
+  Ⲉ: 'E as in Let',
   Ⲏ: 'long EE as in Feet',
-  Ⲓ: 'short I as in fit & Y as in yo',
+  Ⲓ: 'short I as in Fit & Y as in Yo',
+  Ⲕ: 'K',
+  Ⲙ: 'M',
   Ⲛ: 'N',
+  Ⲟ: 'Short O as in Fog',
   Ⲣ: 'R',
+  Ⲩ: 'y as in Hymn',
+  Ⲱ: 'Long O as in Hope',
   ⲞⲨ: 'OO as in Food',
 };
