24cca2c47e7b24c14c6b4ec77da2e76c87bb20c8
Mark Yacoub <markyacoub@google.com>
2022-01-06 14:16:52 -0500

    1:1 Create lesson 1 and the skeleton for letter learning
    Create consts that has all letter names and pronunciations and names.
    Create lesson 1 plan based on the consts and put them in a lesson screen waiting on plans to render.

---
+++ src/screens/level1/learningLetters.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import {View, Text, TextInput} from 'react-native';
+
+import PropTypes from 'prop-types';
+
+import Level1Curriculum from '../../curriculum/level1/learningLetters';
+
+const Level1LearningLetters = ({lessonNumber}) => {
+  const lessonPlan = Level1Curriculum[lessonNumber];
+  if (!lessonPlan) {
+    console.error('No lesson plan for lesson number', lessonNumber);
+    return null;
+  }
+
+  const renderLetters = letters => {
+    const lettersViews = [];
+    // Render Header
+    lettersViews.push(
+      <View key={'header'} style={{flexDirection: 'row'}}>
+        <Text>Letter</Text>
+        <Text>Letter Name</Text>
+        <Text>Letter pronunciation</Text>
+        <Text>Practice typing</Text>
+      </View>,
+    );
+    // Render Letters
+    for (const [letter, letterExplanation] of Object.entries(letters)) {
+      lettersViews.push(
+        <View key={letter} style={{flexDirection: 'row'}}>
+          <Text>{letter}</Text>
+          <Text>{letterExplanation.name}</Text>
+          <Text>{letterExplanation.pronunciation}</Text>
+          <TextInput placeholder='type letter here'></TextInput>
+        </View>,
+      );
+    }
+    return lettersViews;
+  };
+
+  const renderPracticeWords = practiceWords => {
+    const practiceWordsViews = [];
+    // Render Header
+    practiceWordsViews.push(
+      <View key={'header'} style={{flexDirection: 'row'}}>
+        <Text>Word</Text>
+        <Text>Practice typing</Text>
+        <Text>Practice pronunciation</Text>
+      </View>,
+    );
+    // Render Practice Words
+    for (const practiceWord of practiceWords) {
+      practiceWordsViews.push(
+        <View key={practiceWord.word} style={{flexDirection: 'row'}}>
+          <Text>{practiceWord.word}</Text>
+          <TextInput placeholder='type word here'></TextInput>
+          <TextInput placeholder='type pronunciation here'></TextInput>
+        </View>,
+      );
+    }
+    return practiceWordsViews;
+  };
+
+  const renderPracticePhrases = practicePhrases => {
+    const practicePhrasesViews = [];
+    // Render Header
+    practicePhrasesViews.push(
+      <View key={'header'} style={{flexDirection: 'row'}}>
+        <Text>Phrase</Text>
+        <Text>Translation</Text>
+        <Text>Practice pronunciation</Text>
+      </View>,
+    );
+    for (const practicePhrase of practicePhrases) {
+      practicePhrasesViews.push(
+        <View key={practicePhrase.phrase} style={{flexDirection: 'row'}}>
+          <Text>{practicePhrase.phrase}</Text>
+          <Text>{practicePhrase.translation}</Text>
+          <TextInput placeholder='type pronunciation here'></TextInput>
+        </View>,
+      );
+    }
+    return practicePhrasesViews;
+  };
+
+  return (
+    <>
+      {renderLetters(lessonPlan.letters)}
+      {renderPracticeWords(lessonPlan.practiceWords)}
+      {renderPracticePhrases(lessonPlan.practicePhrases)}
+    </>
+  );
+};
+
+Level1LearningLetters.propTypes = {
+  lessonNumber: PropTypes.number.isRequired,
+};
+
+export default Level1LearningLetters;
