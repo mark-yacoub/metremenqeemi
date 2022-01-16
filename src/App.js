cd4940ecb872c28632c1d3017d7ca08b44e9bb53
zakim <Marquos.Zaki@pointclickcare.com>
2022-01-15 20:11:40 -0500

    Added level overview screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import {Button, View} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
+import {Screen} from 'react-native-screens';
+import Level1Intro from '../src/screens/level1/intro';
 
-import LessonBase from './screens/lessonBase';
+import LeessonsList from './screens/LessonsList';
+import Lesson from './screens/lessonBase';
 
 const HomeScreen = ({navigation}) => (
   <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
@@ -36,18 +40,32 @@ const About = ({navigation}) => (
 );
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='Home'>
-        <Drawer.Screen name='Home' component={HomeScreen} />
-        <Drawer.Screen name='Level One' component={LessonBase} />
-        <Drawer.Screen name='Level Two' component={LevelTwo} />
-        <Drawer.Screen name='Level Three' component={LevelThree} />
-        <Drawer.Screen name='Level Four' component={LevelFour} />
-        <Drawer.Screen name='About' component={About} />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Drawer Menu'
+          component={DrawerNavigation}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name='Lesson 0' component={Level1Intro} options={{headerShown: true}} />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+export function DrawerNavigation() {
+  return (
+    <Drawer.Navigator initialRouteName='Home'>
+      <Drawer.Screen name='Home' component={HomeScreen} />
+      <Drawer.Screen name='Level One' component={LeessonsList} />
+      <Drawer.Screen name='Level Two' component={Level1Intro} />
+      <Drawer.Screen name='Level Three' component={LevelThree} />
+      <Drawer.Screen name='Level Four' component={LevelFour} />
+      <Drawer.Screen name='About' component={About} />
+    </Drawer.Navigator>
   );
 }
